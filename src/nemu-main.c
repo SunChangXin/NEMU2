95daccd8bc022e112c1ab9baa5ac7c084d42559b
2022210558-Sun Changxin <3066850676@qq.com>
2024-05-22 12:04:20 +0800

    5.225.225.225.225.225.225.225.225.225.225.225.225.225.225.225.225.225.225.225.225.225.22

--- src/nemu-main.c
+++ src/nemu-main.c
@@ -33,71 +33,9 @@ int main(int argc, char *argv[])
   init_monitor(argc, argv);
 #endif
 
-  FILE *fp;
-  char line[1024]; // 根据最长行的长度调整大小
-  int result, calculated_result;
-  char expression[1024]; // 用于存储表达式字符串
-
-  fp = fopen("tools/gen-expr/input", "r");
-  if (fp == NULL)
-  {
-    perror("Error opening file");
-    return -1;
-  }
-
-  // 逐行读取文件内容
-  while (fgets(line, sizeof(line), fp))
-  {
-    // 去除可能的换行符
-    line[strcspn(line, "\n")] = 0;
-
-    // 找到空格分割结果和表达式
-    char *endPtr;
-    result = (int)strtol(line, &endPtr, 10); // 将结果转换为数字
-    if (*endPtr == ' ')
-    {
-      // 跳过空格
-      endPtr++;
-      // 复制表达式到单独的字符串
-      strncpy(expression, endPtr, sizeof(expression) - 1);
-      expression[sizeof(expression) - 1] = '\0';
-
-      // 去除表达式末尾的换行符
-      expression[strcspn(expression, "\n")] = 0;
-
-      bool success = false;
-
-      // 调用expr函数计算表达式的值
-      calculated_result = expr(expression, &success);
-      if (success)
-      {
-        // 比较结果
-        if (calculated_result == result)
-        {
-          printf("Test passed for expression: '%s'\n", expression);
-        }
-        else
-        {
-          printf("Test failed for expression: '%s' - Expected: %d, Calculated: %d\n", expression, result, calculated_result);
-        }
-      }
-      else
-      {
-        Log("求值时发生错误.\n");
-        assert(0);
-      }
-    }
-    else
-    {
-      printf("Error parsing line: '%s'\n", line);
-    }
-  }
-
-  // 关闭文件
-  fclose(fp);
-
-  /* Start engine.   函数体在 src/engine/interpreter/init.c */
+    /* Start engine.   函数体在 src/engine/interpreter/init.c */
   engine_start();
 
   return is_exit_status_bad(); // src/utils/state.c
 }
+
